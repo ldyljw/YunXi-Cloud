4b2e8d5a177de0b781d01c2c9892a88c1acbfdbb
lidy <lidingyin520@163.com>
2024-05-06 11:38:14 +0800

    test 单元测试插件

---
+++ yunxi-framework/yunxi-spring-boot-starter-test/src/main/java/com/yunxi/framework/test/core/ut/BaseMockitoUnitTest.java
@@ -0,0 +1,16 @@
+package com.yunxi.framework.test.core.ut;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * <p>
+ * 纯 Mockito 的单元测试
+ * </p>
+ *
+ * @author lidy
+ * @since 2024-05-06
+ */
+@ExtendWith(MockitoExtension.class)
+public class BaseMockitoUnitTest {
+}
